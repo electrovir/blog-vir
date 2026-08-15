868f0c3c242af7fb8280ea3978ef13c93fa7992d
electrovir <electrovir@users.noreply.github.com>
2026-08-15 12:34:10 +0000

    [dev] add root GH pages index.html

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,3 +10,4 @@ mkdir -p dist-pages/docs dist-pages/demo
 cp -R packages/blog-vir/dist-docs/. dist-pages/docs
 cp -R packages/demo/dist/. dist-pages/demo
 cp packages/blog-vir/dist-docs/.nojekyll dist-pages/.nojekyll
+cp index.html dist-pages/index.html
