d984a5cf92c766654890bbdbcc44b3100b8e6dad
electrovir <electrovir@users.noreply.github.com>
2026-08-15 11:50:52 +0000

    [patch] fix cli command

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+npm run compile
+npm run docs
+npm run build --workspace @blog-vir/demo
+mkdir -p dist-pages/docs dist-pages/demo
+cp -R packages/blog-vir/dist-docs/. dist-pages/docs
+cp -R packages/demo/dist/. dist-pages/demo
+cp packages/blog-vir/dist-docs/.nojekyll dist-pages/.nojekyll
